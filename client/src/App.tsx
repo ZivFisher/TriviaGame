5508b04ca1f11ec78dc50104c0a6e1dc02561d01
hilma <yotam.hanoch@gmail.com>
2023-02-19 18:13:33 +0200

    mixin file

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,17 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.scss';
+import { Route, Routes } from 'react-router-dom';
 
 
 function App() {
   return (
     <div className="App">
+      <Routes>
+    {/* <Route path='/' element={< Main /> }> 
+    
+    </Route> */}
+      </Routes>
 
     </div>
   );
