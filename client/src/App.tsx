050d71024aa5c3da981f485246791998eb55605e
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 <fisher.ziv@gmail.com>
2023-02-19 15:36:16 +0200

    removing unnecessary code

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,10 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.scss';
-import Button from './components/Button';
 
 function App() {
   return (
     <div className="App">
-      <Button />
     </div>
   );
 }
