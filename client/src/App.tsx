f616c190b2e99b6a7d7d6e798fd85551c16c08d2
hilma <jonathanco999@gmail.com>
2023-02-19 16:14:51 +0200

    removed button component from app.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.scss';
-import Button from './components/Button';
+
 
 function App() {
   return (
     <div className="App">
-      <Button />
+
     </div>
   );
 }
