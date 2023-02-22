7595484848153dba92a11757d534bf18eedc1769
hilma <jonathanco999@gmail.com>
2023-02-22 09:43:16 +0200

    imported Main component to app.tsx. idk why the pull didnt do it itself

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,17 +2,17 @@ import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import { CreateQuiz } from './pages/CreateQuiz';
 import './App.scss';
-import { HomePage } from './pages/home-page/HomePage';
+import { Main } from './pages/main/Main';
 
 
 function App() {
   return (
     <div className="App">
       <Routes>
-        {/* <Route path='/' element={< Main /> }> 
+        <Route path='/' element={< Main /> }> 
     
         <Route path='/create-quiz' element={<CreateQuiz />}></Route>
-    </Route> */}
+    </Route>
       </Routes>
     </div>
   );
