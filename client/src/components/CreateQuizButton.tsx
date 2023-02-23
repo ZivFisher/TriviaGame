6dc74feeed8a186049c1c17bc3c609af6164ec53
yotam-hanoch <118812504+yotam-hanoch@users.noreply.github.com>
2023-02-23 08:58:53 +0200

    G4 60 mobile header (#4)
    * customizable mui button
    
    * G4-19 added main & header components
    
    * added MobileHeader component
    
    * added MobileHeader component
    
    * fixed issues
    
    * fixed issues
    
    ---------
    
    Co-authored-by: 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 <fisher.ziv@gmail.com>

---
+++ client/src/components/CreateQuizButton.tsx
@@ -0,0 +1,29 @@
+import * as React from 'react';
+import { styled } from '@mui/material/styles';
+import Button from '@mui/material/Button';
+
+const CustomButton = styled(Button)({
+    boxShadow: 'none',
+    textTransform: 'none',
+    fontSize: 16,
+    padding: '1vh 4vh',
+    border: '1px solid',
+    borderRadius: '8px',
+    lineHeight: 1.5,
+    backgroundColor: '#378381',
+    fontWeight: 'bold',
+    '&:hover': {
+        backgroundColor: '#0069d9',
+        borderColor: '#0062cc',
+        boxShadow: 'none',
+    },
+});
+
+export default function CreateQuizButton() {
+    return (
+        <CustomButton variant="contained">
+            יצירת חידון
+        </CustomButton>
+    );
+}
+
---
+++ client/src/components/mobile-header/MobileHeader.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import './MobileHeader.scss';
+
+interface MyProps {
+    title: string;
+    showLogo: boolean;
+}
+
+export const MobileHeader: React.FC<MyProps> = ({ title, showLogo }) => {
+    return (
+        <header className='generic-mobile-header'>
+            <img src="./svg/back.svg" alt="" />
+            <h1>{title}</h1>
+            {showLogo ?
+                <img
+                    src="./svg/monkey-with-laptop.svg"
+                    alt="monkey with laptop"
+                /> :
+                <div className='empty-div' />}
+        </header>
+    )
+}
