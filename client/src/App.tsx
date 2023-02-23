a44c07958cf1254725f0ce6e320f54da3b5ebe09
yotam-hanoch <118812504+yotam-hanoch@users.noreply.github.com>
2023-02-23 10:02:52 +0200

    G4-66 loading process component (#10)
    * G4-66 loading process
    
    * G4-66 fix condition

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
+import { Loading } from './pages/loading/Loading';
 import { NotFoundContent } from './pages/not-found/NotFound';
 import { CreateQuiz } from './pages/CreateQuiz';
 import './App.scss';
@@ -12,12 +13,13 @@ function App() {
     <div className="App">
       <Routes>
         <Route path='/' element={< Main />}>
+          <Route path='/loading' element={<Loading />}></Route>
           <Route path='/create-quiz' element={<CreateQuiz />}></Route>
           <Route path='home-page' element={<HomePage />}></Route>
           <Route path='/*' element={<NotFoundContent />}></Route>
         </Route>
       </Routes>
-    </div>
+    </div >
   );
 }
 
