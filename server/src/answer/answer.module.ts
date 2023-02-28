fc361119a11451f095e414d41097b6b68adefff9
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 <fisher.ziv@gmail.com>
2023-02-28 15:01:33 +0200

    added import for answer.module.ts

--- server/src/answer/answer.module.ts
+++ server/src/answer/answer.module.ts
@@ -1,9 +1,17 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AnswerController } from './answer.controller';
+import { Answer } from './answer.entity';
 import { AnswerService } from './answer.service';
 
 @Module({
   controllers: [AnswerController],
-  providers: [AnswerService]
+  providers: [AnswerService],
+  exports: [AnswerService],
+  imports: [
+    TypeOrmModule.forFeature([
+      Answer
+    ])
+  ]
 })
 export class AnswerModule { }
