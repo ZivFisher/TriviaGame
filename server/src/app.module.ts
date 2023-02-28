7357e7f3ee45ee04f4caad5e346d0c95da90ece2
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 <fisher.ziv@gmail.com>
2023-02-28 16:11:18 +0200

    fixed issues

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,21 +1,13 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { DatabaseModule } from './database.module';
 import { QuizModule } from './quiz/quiz.module';
-import { ScoreModule } from './score/score.module';
-import { AnswerModule } from './answer/answer.module';
-import { QuestionModule } from './question/question.module';
 
 @Module({
   imports: [
     DatabaseModule,
     QuizModule,
-    ScoreModule,
-    AnswerModule,
-    QuestionModule
   ],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [],
+  providers: [],
 })
 export class AppModule { }
