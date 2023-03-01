540c9a614092b009acf758b9cfa845ea4ce7b5dc
ziv fisher <73037999+zivf1990@users.noreply.github.com>
2023-03-01 12:30:59 +0200

    G4 81 create quiz result page (#19)
    * G4-81 added quiz result page for large screens
    
    * G4-81 added mobile quiz result page
    
    * G4-81 added mobile quiz result page little change
    
    * G4-81 added style
    
    * remove package-lock from home folder
    
    * edite app.tsx routes
    
    * G4-81 added a comment

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,11 @@
 import { Route, Routes } from 'react-router-dom';
-import { Loading } from './pages/loading/Loading';
 import { NotFoundContent } from './pages/not-found/NotFound';
 import { CreateQuiz } from './pages/create-quiz-page/CreateQuiz';
 import { HomePage } from './pages/home-page/HomePage';
 import { Main } from './pages/main/Main';
 import { PlayQuizPage } from './pages/play-quiz-page/PlayQuizPage';
 import { QuizNickname } from './pages/quiz-nickname/QuizNickname';
+import { QuizResults } from './pages/quiz-results/QuizResults';
 import { MyQuizzes } from './pages/my-quizzes/MyQuizzes';
 import { QuizDetailsProvider } from './contexts/quizDetailsContext';
 import { Register } from './pages/login-register/register/Register';
@@ -15,36 +15,29 @@ import { PlayQuizProvider } from "./contexts/PlayQuizContext";
 import './App.scss';
 
 
-
-
-
-
-
 function App() {
   return (
     <div className="App">
-      <Routes>
-        <Route path='/' element={< Main />}>
-          <Route path='/loading' element={<Loading />}></Route>
-          <Route path='/create-quiz' element={
-            <QuizDetailsProvider>
-              <CreateQuiz />
-            </QuizDetailsProvider>}
-          ></Route>
-          <Route path='home-page' element={<HomePage />}></Route>
-          <Route path='login' element={<Login />}></Route>
-          <Route path='register' element={<Register />}></Route>
-          <Route path='quiz-nickname' element={<QuizNickname />}></Route>
-          <Route path='play-quiz' element={
-            <PlayQuizProvider>
-              <PlayQuizPage />
-            </PlayQuizProvider>}
-          ></Route>
-          <Route path='/*' element={<NotFoundContent />}></Route>
-          <Route path='my-quizzes' element={<MyQuizzes />}></Route>
-          <Route path='score-board' element={<ScorePage />}></Route>
-        </Route>
-      </Routes>
+      <PlayQuizProvider>
+        <Routes>
+          <Route path='/' element={< Main />}>
+            <Route path='/create-quiz' element={
+              <QuizDetailsProvider>
+                <CreateQuiz />
+              </QuizDetailsProvider>}
+            ></Route>
+            <Route path='home-page' element={<HomePage />} />
+            <Route path='login' element={<Login />} />
+            <Route path='register' element={<Register />} />
+            <Route path='quiz-nickname' element={<QuizNickname />} />
+            <Route path='quiz-results' element={<QuizResults />} />
+            <Route path='play-quiz' element={<PlayQuizPage />} />
+            <Route path='score-board' element={<ScorePage />} />
+            <Route path='my-quizzes' element={<MyQuizzes />}></Route>
+            <Route path='/*' element={<NotFoundContent />} />
+          </Route>
+        </Routes>
+      </PlayQuizProvider>
     </div >
   );
 }
